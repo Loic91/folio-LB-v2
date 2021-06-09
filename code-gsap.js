d4da5f627c7fedf614e88881f6ffae58cd822865
Loic91 <62215559+Loic91@users.noreply.github.com>
2021-06-10 01:20:25 +0200

    First commit

---
+++ code-gsap.js
@@ -0,0 +1,22 @@
+gsap.from('#text-0', 1.5, {
+    y: '100%',
+    delay: 0.9
+})
+
+gsap.from('#text-1', 1.5, {
+    y: '100%',
+    delay: 1.2
+})
+
+gsap.from('#text-2', 1.5, {
+    y: '100%',
+    delay: 1.5
+})
+
+gsap.from('#text-3', 1.5, {
+    y: '100%',
+    delay: 1.8
+})
+
+
+
